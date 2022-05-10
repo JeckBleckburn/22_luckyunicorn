b2a0be6df06ea4a2ab29fa25b3ecad75bcbf6ed7
JeckBleckburn <blackb.jack@awatapu.school.nz>
2022-05-11 09:12:41 +1200

    added if statement

--- 03_instructions1.py
+++ 03_instructions1.py
@@ -15,15 +15,26 @@ def yes_no(question):
         else:
             print("Please answer yes / no")
 
-def instructions():
-    print("----How To Play----")
-    print("")
-    print("")
-    print("")
+
+def instructions(question):
+    print("Here are the Instructions")
+
+    helpneed = input(question).lower().strip()
+
+    if helpneed == "Yes" or helpneed == "Y":
+        print("That's ok here you go")
+        instructions()
+
+    else:
+        print("Ok that good lets start the game")
 
 
-show_instructions = yes_no("Have you played this game before? ").lower().strip()
-if show_instructions == "yes":
+morehelp = instructions("are you ok with the rules? >>> Y/N")
+
+show_instructions = yes_no("Have you ever played this game before?").lower().strip()
+
+if show_instructions == "no":
     instructions()
+
 else:
-    print("Well since you're Sooooo smart just play then\n Don't Come crying to me")
+    print("Well lets get you started then")
