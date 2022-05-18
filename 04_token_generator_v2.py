33cfd6876fc88536d0abc45a075d29f826aff878
JeckBleckburn <blackb.jack@awatapu.school.nz>
2022-05-18 12:40:05 +1200

    while statment added to rerun the code until they have no more money left

--- 04_token_generator_v2.py
+++ 04_token_generator_v2.py
@@ -1,8 +1,9 @@
 from time import sleep
 import random
 
+
 def num_check(question, lowest, highest,):
-    dumb = "Please enter a amount between 1 and 10 plz"
+    error = "Please enter a amount between 1 and 10 plz"
 
     valid = False
     while not valid:
@@ -10,42 +11,48 @@ def num_check(question, lowest, highest,):
             bank = int(input(question))
 
             if lowest < bank <= highest:
-                print("thank you.\nYou have asked to play with ${}".format(bank))
+                print("You have asked to play with ${}".format(bank))
                 return bank
 
         except ValueError:
-            print(dumb)
+            print(error)
+
 
 
 balance = num_check("How much would you like to pay With\n>>>", 0, 10,)
 sleep(1)
 
-token = ["unicorn", "horse", "horse", "horse", "zebra", "donkey", "donkey", "donkey", "donkey"]
-token_gen = True
-while token_gen:
-    try:
-        rounds = int(input("how many rounds would you like to play"))
-        if rounds <= balance:
-            for item in range(1, rounds + 1):
-                chosen_num = random.choice(token)
 
-                if chosen_num == "unicorn":
-                    balance += 4
-                elif chosen_num == "donkey":
-                    balance -= 1
-                else:
-                    balance -= .5
+def token_game(balance):
+    token = ["unicorn", "horse", "horse", "horse", "zebra", "donkey", "donkey", "donkey", "donkey"]
+    token_gen = True
+    while token_gen:
+        try:
+            rounds = int(input("--How many rounds would you like to play--"))
+            if rounds <= balance:
+                for item in range(1, rounds + 1):
+                    chosen_num = random.choice(token)
 
-                print("-------------------------")
-                print("Token: {} , Balance: ${}".format(chosen_num, balance))
-                print("-------------------------")
-                sleep(.5)
+                    if chosen_num == "unicorn":
+                        balance += 4
+                    elif chosen_num == "donkey":
+                        balance -= 1
+                    else:
+                        balance -= .5
 
-                if balance <= .5:
-                    token_gen = False
+                    print("Token: {} , Balance: ${}".format(chosen_num, balance))
+                    print("-------------------------")
+                    sleep(.5)
 
-    except ValueError:
-            print("must be equal or lower than your balance")
+                    if balance <= .5:
+                        token_gen = False
+
+        except ValueError:
+                print("Must be equal or lower than your balance")
+
+
+token_game(balance)
+
 
 
 
